a727ebda327ee5508082342e1dde64b53a4c404e
16005107 <theo.laperrouse@etudiant.univ-rennes1.fr>
2021-04-06 20:21:11 +0200

    just one snapshoot

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -6,27 +6,9 @@
 //       .should("have.length", 200);
 //    });
 
-describe("Menu", function() {
-  it("click on all tabs", function() {
+describe("Snap", function() {
+  it("Take a screenshoot of dashboard", function() {
     cy.visit("http://localhost:8080/#/dashboard");
     cy.percySnapshot("Dashboard Snapshot");
-    cy.get(
-      ".sidebar-wrapper > .nav > .nav-item:nth-child(2) > .nav-link > p"
-    ).click();
-    cy.percySnapshot("Icons Snapshot");
-    cy.get(
-      ".sidebar-wrapper > .nav > .nav-item:nth-child(3) > .nav-link > p"
-    ).click();
-    cy.get(
-      ".content > .card > .card-body > .form-group > .form-control"
-    ).click();
-    cy.get(".content > .card > .card-body > .form-group > .form-control").type(
-      "du texte"
-    );
-    cy.percySnapshot("Maps Snapshot");
-    cy.get(
-      ".sidebar-wrapper > .nav > .nav-item:nth-child(4) > .nav-link > p"
-    ).click();
-    cy.percySnapshot("Notif Snapshot");
-  });
+   });
 });
