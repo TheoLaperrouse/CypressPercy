202fcf9607d6b2a01582b27fdf0c0ab1206a2fc4
16005107 <theo.laperrouse@etudiant.univ-rennes1.fr>
2021-04-06 20:16:32 +0200

    some tests

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,23 +1,32 @@
 /* eslint-disable no-undef */
 
-
 //   it("Menu should have 8 children", () => {
 //     cy.get(".sidebar-wrapper > .nav")
 //       .children()
 //       .should("have.length", 200);
 //    });
 
-describe('Menu', function() {
-  it('click on all tabs', function() {
-     cy.visit('http://localhost:8080/#/dashboard')
-     cy.percySnapshot('Dashboard Snapshot');
-     cy.get('.sidebar-wrapper > .nav > .nav-item:nth-child(2) > .nav-link > p').click()
-     cy.percySnapshot('Icons Snapshot');
-     cy.get('.sidebar-wrapper > .nav > .nav-item:nth-child(3) > .nav-link > p').click()
-     cy.get('.content > .card > .card-body > .form-group > .form-control').click()
-     cy.get('.content > .card > .card-body > .form-group > .form-control').type('du texte')
-     cy.percySnapshot('Maps Snapshot');
-     cy.get('.sidebar-wrapper > .nav > .nav-item:nth-child(4) > .nav-link > p').click()
-     cy.percySnapshot('Notif Snapshot');
-  })
- })
+describe("Menu", function() {
+  it("click on all tabs", function() {
+    cy.visit("http://localhost:8080/#/dashboard");
+    cy.percySnapshot("Dashboard Snapshot");
+    cy.get(
+      ".sidebar-wrapper > .nav > .nav-item:nth-child(2) > .nav-link > p"
+    ).click();
+    cy.percySnapshot("Icons Snapshot");
+    cy.get(
+      ".sidebar-wrapper > .nav > .nav-item:nth-child(3) > .nav-link > p"
+    ).click();
+    cy.get(
+      ".content > .card > .card-body > .form-group > .form-control"
+    ).click();
+    cy.get(".content > .card > .card-body > .form-group > .form-control").type(
+      "du texte"
+    );
+    cy.percySnapshot("Maps Snapshot");
+    cy.get(
+      ".sidebar-wrapper > .nav > .nav-item:nth-child(4) > .nav-link > p"
+    ).click();
+    cy.percySnapshot("Notif Snapshot");
+  });
+});
