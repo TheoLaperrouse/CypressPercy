4ffd92d66ffdf23dfa8dd96fcbc5294d74e70a12
16005107 <theo.laperrouse@etudiant.univ-rennes1.fr>
2021-04-06 20:06:48 +0200

    Test Good for Demo #2

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -7,22 +7,17 @@
 //       .should("have.length", 200);
 //    });
 
-// describe('Menu', function() {
-//   it('click on all tabs', function() {
-//      cy.viewport(1848, 980)
-//      cy.visit('http://localhost:8080/#/dashboard')
-//      cy.percySnapshot('Dashboard Snapshot');
-//      cy.get('.sidebar-wrapper > .nav > .nav-item:nth-child(2) > .nav-link > p').click()
-//      cy.percySnapshot('Icons Snapshot');
-//      cy.get('.sidebar-wrapper > .nav > .nav-item:nth-child(3) > .nav-link > p').click()
-//      cy.get('.content > .card > .card-body > .form-group > .form-control').click()
-//      cy.get('.content > .card > .card-body > .form-group > .form-control').type('du texte')
-//      cy.percySnapshot('Maps Snapshot');
-//      cy.get('.sidebar-wrapper > .nav > .nav-item:nth-child(4) > .nav-link > p').click()
-//      cy.percySnapshot('Notif Snapshot');
-//      cy.get('.sidebar-wrapper > .nav > .nav-item:nth-child(5) > .nav-link > p').click()
-//      cy.get('.sidebar-wrapper > .nav > .nav-item:nth-child(6) > .nav-link > p').click()
-//      cy.get('.sidebar-wrapper > .nav > .nav-item:nth-child(7) > .nav-link > p').click()
-//      cy.get('.sidebar-wrapper > .nav > .nav-item:nth-child(8) > .nav-link > p').click()
-//   })
-//  })
+describe('Menu', function() {
+  it('click on all tabs', function() {
+     cy.visit('http://localhost:8080/#/dashboard')
+     cy.percySnapshot('Dashboard Snapshot');
+     cy.get('.sidebar-wrapper > .nav > .nav-item:nth-child(2) > .nav-link > p').click()
+     cy.percySnapshot('Icons Snapshot');
+     cy.get('.sidebar-wrapper > .nav > .nav-item:nth-child(3) > .nav-link > p').click()
+     cy.get('.content > .card > .card-body > .form-group > .form-control').click()
+     cy.get('.content > .card > .card-body > .form-group > .form-control').type('du texte')
+     cy.percySnapshot('Maps Snapshot');
+     cy.get('.sidebar-wrapper > .nav > .nav-item:nth-child(4) > .nav-link > p').click()
+     cy.percySnapshot('Notif Snapshot');
+  })
+ })
